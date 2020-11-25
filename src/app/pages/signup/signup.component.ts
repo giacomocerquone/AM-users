abf39a4b20b8a834f25be2a0d1bb70dd31e65c8c
giacomocerquone <cerquone96@hotmail.it>
2020-11-26 00:42:38 +0100

    add signup component

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+type signupDataType = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+};
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  signupData: signupDataType = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [SignupComponent],
   imports: [
     CommonModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
   ]
 })
 export class SignupModule { }
